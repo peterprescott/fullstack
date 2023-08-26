057c92b317319de250fc0f5c3897af326e06ca24
Peter Prescott <prescott.peter@gmail.com>
2023-08-26 15:35:29 +0100

    Refactor run.py script
    Refactor to make port-finding function a decorator.
    And rename functions for clearer frontend/backend symmetry.
    Also use multiprocessing instead of threading.
    And use black, isort, and have full docstring coverage.

--- run.py
+++ run.py
@@ -1,43 +1,93 @@
+"""
+Run the frontend and backend servers in parallel.
+"""
+
+import multiprocessing
 import os
-import threading
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-import socketserver
 
 from api.api import app
 
-FRONTEND_FOLDER = os.path.join('frontend', 'ui')
+FRONTEND_FOLDER = os.path.join("frontend", "ui")
 
-def run_flask_app():
-    app.run(host='localhost', port=5000)
 
-def serve(port = 8000, address="localhost", max_tries=3):
-    
+def serve_on_available_port(func: callable):
+    """
+    Decorator to try to start a server on a port, and if it fails, try the next
+    port until it succeeds or the max_tries limit is reached.
+    """
+
+    def wrapper(*args, **kwargs):
+        port = kwargs.get("port", 8000)  # Default port value
+        max_tries = kwargs.get(
+            "max_tries", 10
+        )  # Default max_tries value
+        i = 0
+        while i < max_tries:
+            try:
+                return func(*args, **kwargs)
+            except OSError:
+                print(f"\nPort {port} is busy...")
+                port += 1
+                i += 1
+
+        if i == max_tries:
+            print(
+                f"Reached attempt limit of {max_tries}, "
+                "try starting with a different port"
+            )
+        return None
+
+    return wrapper
+
+
+def create_server(site_folder: str, address: str, port: int):
+    """
+    Create a server to serve the files in the site_folder directory.
+    """
+
     class Handler(SimpleHTTPRequestHandler):
+        """
+        Handler class to serve the files in the site_folder directory.
+        """
+
         def __init__(self, *args, **kwargs):
-            super().__init__(directory=FRONTEND_FOLDER, *args, **kwargs)
+            super().__init__(directory=site_folder, *args, **kwargs)
 
-    i = 0
-    while i < max_tries:
-        try:
-            server = HTTPServer((address, port), Handler)
-            print(f'\nFrontend is live at https://{address}:{port}')
-            server.serve_forever()
-        except OSError:
-            print(f'\nPort {port} is busy...')
-            port += 1
-            i += 1
+    return HTTPServer((address, port), Handler)
 
-    if i == max_tries:
-        print(f'Reached attempt limit of {max_tries}, try starting with a different port')
 
-# Create threads for both functions
-flask_thread = threading.Thread(target=run_flask_app)
-static_thread = threading.Thread(target=serve)
+@serve_on_available_port
+def serve_frontend(
+    site_folder=FRONTEND_FOLDER, port=8000, address="localhost"
+):
+    """
+    Serve the frontend on the given port.
+    """
+    server = create_server(site_folder, address, port)
+    print(f"\nFrontend is live at https://{address}:{port}")
+    server.serve_forever()
 
-# Start the threads
-flask_thread.start()
-static_thread.start()
 
-# Wait for both threads to finish
-flask_thread.join()
-static_thread.join()
+@serve_on_available_port
+def serve_backend(port=5000, address="localhost"):
+    """
+    Serve the backend on the given port.
+    """
+    print(f"\nBackend is live at https://{address}:{port}")
+    app.run(host=address, port=port)
+
+
+if __name__ == "__main__":
+    frontend_process = multiprocessing.Process(
+        target=serve_frontend, kwargs={"port": 8000}
+    )
+    backend_process = multiprocessing.Process(
+        target=serve_backend, kwargs={"port": 5000}
+    )
+
+    frontend_process.start()
+    backend_process.start()
+
+    frontend_process.join()
+    backend_process.join()
